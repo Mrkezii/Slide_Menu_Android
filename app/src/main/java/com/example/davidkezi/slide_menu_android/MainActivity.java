99d5cc6bc0acd3709cff0b2f4376b2591aa490e7
mrkezii <david.kezi@yahoo.com>
2016-03-11 11:38:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/davidkezi/slide_menu_android/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.davidkezi.slide_menu_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
